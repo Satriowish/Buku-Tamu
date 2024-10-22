0437a00357d0438bac523e645bb78327e97c512b
Satriowish <satriowisnuap@gmail.com>
2024-10-22 11:16:08 +0700

    membuat form login

---
+++ admin/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bullworth Academy</title>
+  <!-- menambahkan icon -->
+  <link rel="icon" type="image/png/jpg" href="" sizes="32x32">
+  <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@400;700&family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+  <!-- menambahkan style -->
+  <link rel="stylesheet" href="">
+</head>
+
+<body>
+
+  <div class="box-login">
+    <h2>Login</h2>
+    <!-- form login -->
+    <form action="" method="POST">
+      <input type="text" name="user" placeholder="Username" class="input-control">
+      <input type="password" name="pass" placeholder="Password" class="input-control">
+      <input type="submit" name="submit" value="Login" class="login-btn">
+    </form>
+
+  </div>
+</body>
+</html>
