7afd7acc70f300fd35e1b16820dc030ba9da1ee2
Satriowish <satriowisnuap@gmail.com>
2024-10-24 20:24:33 +0700

    update

--- admin/tamu/edit-tamu.php
+++ admin/tamu/edit-tamu.php
@@ -49,60 +49,73 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin | Edit Tamu</title>
     <link rel="icon" type="image/png/jpg" href="../../img/icon.png" sizes="32x32">
+    <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../../css/page-edit-tamu.css">
 </head>
 <body>
     <header>
-        <h1>Edit Tamu Bullworth</h1>
-        <ul>
-            <li><a href="../dashboard.php">Dashboard</a></li>
-            <li><a href="../profil.php">Profil</a></li>
-            <li><a href="../history.php">Riwayat</a></li>
-        </ul>
+        <div class="container">
+            <h1>Edit Tamu Bullworth</a></h1>
+            <ul>
+                <li><a href="../dashboard.php">Dashboard</a></li>
+                <li><a href="../profil.php">Profil</a></li>
+                <li><a href="../history.php">Riwayat</a></li>
+            </ul>
+        </div>
     </header>
 
     <div class="section">
-        <h3>Edit Data Tamu</h3>
-        <div class="box">
-            <form method="POST">
-                <label>Nama Tamu:</label>
-                <input type="text" name="tamu_name" value="<?php echo $row['tamu_name']; ?>" required>
+        <div class="container">
+            <h3>Edit Data Tamu</h3>
 
-                <label>Alamat:</label>
-                <input type="text" name="tamu_address" value="<?php echo $row['tamu_address']; ?>" required>
+            <!-- form edit tamu -->
+            <div class="box">
+                <form action="" method="POST">
 
-                <label>Keterangan:</label>
-                <textarea name="keterangan" required><?php echo $row['keterangan']; ?></textarea>
+                    <label for="tamu_name">Nama Tamu:</label>
+                    <input type="text" name="tamu_name" value="<?php echo $row['tamu_name']; ?>" required>
 
-                <label>No. Telp:</label>
-                <input type="text" name="tamu_telp" value="<?php echo $row['tamu_telp']; ?>" required>
+                    <label for="tamu_address">Alamat:</label>
+                    <input type="text" name="tamu_address" value="<?php echo $row['tamu_address']; ?>" required>
 
-                <label>Email:</label>
-                <input type="email" name="tamu_email" value="<?php echo $row['tamu_email']; ?>" required>
+                    <label for="keterangan">Keterangan:</label>
+                    <textarea name="keterangan" required><?php echo $row['keterangan']; ?></textarea>
 
-                <label>Kategori Tamu:</label>
-                <select name="kategori_id" required>
+                    <label for="tamu_telp">No. Telp:</label>
+                    <input type="text" name="tamu_telp" value="<?php echo $row['tamu_telp']; ?>" required>
+
+                    <label for="tamu_email">Email:</label>
+                    <input type="email" name="tamu_email" value="<?php echo $row['tamu_email']; ?>" required>
+
+                    <label for="kategori_id">Kategori Tamu:</label>
+                    <select name="kategori_id" required>
                     <option value="" disabled selected>Pilih Kategori</option>
                     <?php
-                        $kategori_query = sqlsrv_query($conn, "SELECT * FROM tb_kategori");
-                        while ($kategori = sqlsrv_fetch_array($kategori_query, SQLSRV_FETCH_ASSOC)) {
-                            echo "<option value='".$kategori['kategori_id']."' "
-                             . ($kategori['kategori_id'] == $row['kategori_id'] ? 'selected' : '') 
-                             . ">".$kategori['kategori_name']."</option>";
-                        }
+                       $kategori_query = sqlsrv_query($conn, "SELECT * FROM tb_kategori");
+                       while ($kategori = sqlsrv_fetch_array($kategori_query, SQLSRV_FETCH_ASSOC)) {
+                       echo "<option value='".$kategori['kategori_id']."' "
+                       . ($kategori['kategori_id'] == $row['kategori_id'] ? 'selected' : '') 
+                       . ">".$kategori['kategori_name']."</option>";
+                         }
                     ?>
-                </select>
-
-                <input type="submit" value="Simpan Perubahan">
-            </form>
+                   </select>
+                 </select>
+                 
+                    <!-- submit -->
+                    <input type="submit" value="Simpan Perubahan" class="login-btn">
+                </form>
+            </div>
         </div>
     </div>
 
     <footer>
-        <small>Copyright &copy; 2024 - Satrio Wisnu Adi Pratama</small>
+        <div class="container">
+            <small>Copyright &copy; 2024 - Satrio Wisnu Adi Pratama</small>
+        </div>
     </footer>
 </body>
 </html>
